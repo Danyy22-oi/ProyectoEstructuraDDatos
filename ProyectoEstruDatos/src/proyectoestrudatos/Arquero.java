d61046102c6c56ec0d82dbd23423f02b38b252cb
Danyy22-oi <tdanielaaa22@gmail.com>
2023-04-10 20:33:33 -0600

    arquero

--- ProyectoEstruDatos/src/proyectoestrudatos/Arquero.java
+++ ProyectoEstruDatos/src/proyectoestrudatos/Arquero.java
@@ -8,6 +8,26 @@ package proyectoestrudatos;
  *
  * @author tdani
  */
-public class Arquero {
+public class Arquero extends Personaje {
+    public Arquero() {
+		super();
+	}
+
+	public Arquero(String nombre, int ataque, int defensa, int vida) {
+		super(nombre, ataque, defensa, vida);
+	}
+	
+	public int ataqueFlecha() {
+		/*
+		 * Nivel de ataque máximo
+		 */
+		System.out.println(nombre + " usa su ataque de flechas");
+		return (int)(ataque * 1);
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Arquero: %s / Nivel de Vida: %d", nombre, vida);
+	}
     
 }
